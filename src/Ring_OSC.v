52d23a384cc6aeb8f8244fd62cfeea7ac306da8c
fledermaus2018 <38480129+fledermaus2018@users.noreply.github.com>
2024-09-07 03:46:32 +0900

    Update Ring_OSC.v

--- src/Ring_OSC.v
+++ src/Ring_OSC.v
@@ -14,13 +14,19 @@ module RING_OSCILLATOR (
     output wire n7,
     output wire n8,
     output wire n9,
-    output wire n10
+    output wire n10,
+    output wire n11,
+    output wire n12,
+    output wire n13,
+    output wire n14,
+    output wire n15
+
 );
   
 
   // NAND chain (15 stages)
     nand_gate NAND1 (
-    	.A(en1),
+        .A(en0),
     	.B(in),
     	.Y(n1)
     );
@@ -32,7 +38,7 @@ module RING_OSCILLATOR (
     );
     
     nand_gate NAND3 (
-    	.A(en1),
+        .A(en0),
     	.B(n2),
     	.Y(n3)
     );
@@ -44,9 +50,70 @@ module RING_OSCILLATOR (
     );
     
     nand_gate NAND5 (
-    	.A(en1),
+        .A(en0),
     	.B(n4),
+        .Y(n5)
+    );
+    
+    nand_gate NAND6 (
+        .A(en0),
+        .B(n5),
+        .Y(n6)
+    );
+    
+    nand_gate NAND7 (
+    	.A(en0),
+        .B(n6),
+        .Y(n7)
+    );
+    
+    nand_gate NAND8 (
+        .A(en0),
+        .B(n7),
+        .Y(n8)
+    );
+    
+    nand_gate NAND9 (
+    	.A(en0),
+        .B(n8),
+        .Y(n9)
+    );
+    
+    nand_gate NAND10 (
+        .A(en0),
+        .B(n9),
+        .Y(n10)
+    );
+    
+    nand_gate NAND11 (
+        .A(en0),
+        .B(n10),
+        .Y(n11)
+    );
+    
+    nand_gate NAND12 (
+    	.A(en0),
+        .B(n11),
+        .Y(n12)
+    );
+    
+    nand_gate NAND13 (
+        .A(en0),
+        .B(n12),
+        .Y(n13)
+    );
+    
+    nand_gate NAND14 (
+    	.A(en0),
+        .B(n13),
+        .Y(n14)
+    );
+    
+    nand_gate NAND15 (
+        .A(en0),
+        .B(n14),
     	.Y(out)
     );
+        
     
 endmodule
